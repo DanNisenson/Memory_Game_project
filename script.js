74f91541045861417cc9fa430b40c3609746525e
gema <gjcuenca@hotmail.com>
2022-10-31 21:32:09 +0100

    board

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 // grid size
-let rows = 4;
+let rows = 5;
 let columns = 4;
-
-// number pairs to be inserted in <input> elements
-let numbers = []; 
-for (let i = 1; i <= rows * columns / 2; i++) {
-    numbers.push(i, i);
-}
-
-// get <table> element
-let tableElement = document.getElementById('table')
-
-// make table rows, cells and input elements. Append them to HTML.
-for (let i = 1; i <= rows; i++) {
-    let row = document.createElement('tr');
-    row.id = `row${i}`;
-    tableElement.appendChild(row)
-    
-    for (let j = 1; j <= columns; j++) {
-        let cell = document.createElement('td');
-        cell.id = `cell${i}_${j}`;
-        row.appendChild(cell);
-
-        let input = document.createElement('input');
-        input.type = 'button';
-        // give each input an 'id'
-        cell.appendChild(input);
-    }
-}
 
 // Math.floor( Math.random() * (rows * columns / 2) )
 // Math.random -> check if it's included in numbers && not repeated more than once
+
+
+// Math.floor( Math.random() * (rows * columns / 2) )
+// Math.random -> check if it's included in numbers && not repeated more than once
+// realizar al inicio
+//Evento que al cargarse la ventana carga las funciones cargarImagenes, empezarJuego y cargar el reloj
+window.onload = function () {
+    loadBoard();
+}
+
+///Definicion funciones
+
+ function reset(){
+    let refresh = document.getElementById('new-game');
+    refresh.addEventListener('click', _ => {
+            location.reload();
+})
+    loadBoard();
+ }
+
+// number pairs to be inserted in <input> elements
+
+function loadNumbers() {
+let numbers = []; 
+    for (let i = 1; i <= rows * columns / 2; i++) {
+        numbers.push(i,i);
+    }
+    // desordenamos array
+    randon=numbers.sort(function(){return Math.random()-0.5});
+}
+//Load Board
+function loadBoard(){
+    loadNumbers();
+    let boardElement = document.getElementById('board');
+    let num=0
+    // make  board elements and input elements. Append them to HTML.
+    for (let i = 0; i <= (rows*columns-1); i++) {
+        if (i%rows ==0 || i==0) {
+            var row = document.createElement('div');
+            row.className="card";
+            num++
+            row.id = `row${num}`;
+            boardElement.appendChild(row);
+        }
+        let card = document.createElement('input');
+        card.type = 'button';
+        card.id = `card${i}`;
+        card.value=randon[i];
+        row.appendChild(card);
+    }
+}

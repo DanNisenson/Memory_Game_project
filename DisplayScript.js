db8693438d9c621a5c0c30331ea34bdb1731029d
gema <gjcuenca@hotmail.com>
2022-11-07 16:59:13 +0100

    imagenes

---
+++ DisplayScript.js
@@ -0,0 +1,53 @@
+// grid size
+let rows = 5;
+let columns = 4;
+
+// realizar al inicio
+//Evento que al cargarse la ventana carga las funciones cargarImagenes, empezarJuego y cargar el reloj
+window.onload = function () {
+  loadBoard();
+};
+
+///Definicion funciones
+
+function reset() {
+  let refresh = document.getElementById("new-game");
+  refresh.addEventListener("click", (_) => {
+    location.reload();
+  });
+  loadBoard();
+}
+
+// number pairs to be inserted in <input> elements
+
+function loadNumbers() {
+  let numbers = [];
+  for (let i = 1; i <= (rows * columns) / 2; i++) {
+    numbers.push(i, i);
+  }
+  // desordenamos array
+  random = numbers.sort(function () {
+    return Math.random() - 0.5;
+  });
+}
+//Load Board
+function loadBoard() {
+  loadNumbers();
+  let boardElement = document.getElementById("board");
+  let num = 0;
+  // make  board elements and input elements. Append them to HTML.
+  for (let i = 0; i <= rows * columns - 1; i++) {
+    if (i % rows == 0 || i == 0) {
+      var row = document.createElement("div");
+      row.className = "card";
+      num++;
+      row.id = `row${num}`;
+      boardElement.appendChild(row);
+    }
+    let card = document.createElement("input");
+    card.type = "button";
+    card.id = `card${i}`;
+    card.value = random[i];
+    row.appendChild(card);
+  }
+}

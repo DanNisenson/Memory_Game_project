411af3d02ded4a36616b2944ed70874064c96f92
gema <gjcuenca@hotmail.com>
2022-11-11 21:49:24 +0100

    game

--- GameLogic.js
+++ GameLogic.js
@@ -22,12 +22,13 @@ const cardSelect = (e) => {
   selectedCards.push(e.target); // not sure how this evt attribute is working
   // disables selected card
   e.target.setAttribute("disabled", "");
-    //ponemos imagen y guardamos id para cambio imagen despues
+    //Save id to change image later
     const ImgCard=document.getElementById(e.target.id);
+
     ImgCard.src = './img/'+ImgCard.value+'.png';
     selectedCardsId = [ ...selectedCardsId,  ImgCard.id];
   // display value on screen
-  if (selectedCards.length == 2) {
+  if (selectedCards.length === 2) {
     setTimeout(selectionCheck,1000);
   }
 };
@@ -36,33 +37,37 @@ const cardSelect = (e) => {
 const selectionCheck = () => {
     const ImgCard1=document.getElementById(selectedCardsId[0]);
     const ImgCard2=document.getElementById(selectedCardsId[1]);
-    if (selectedCards[0] == selectedCards[1] && turn == 1 ) {
+
+    if (selectedCards[0].value == selectedCards[1].value && turn === 1 ) {
         players.player1++;
         player1Counter.value = players.player1;
         selectedCards = [];
         selectedCardsId = [];
         //ImgCard.src = './img/'+ImgCard.value+'.png'; //ocultar o dejar en blanco
-    } else if (selectedCards[0] == selectedCards[1] && turn == 2) {
+    } else if (selectedCards[0].value === selectedCards[1].value && turn === 2) {
         players.player2++;
         player2Counter.value = players.player2;
-        selectedCards,selectedCardsId = [];
+        selectedCards =[];
+        selectedCardsId = [];
          //ImgCard.src = './img/'+ImgCard.value+'.png'; //ocultar o dejar en blanco
-    } else if (turn == 1) {
+    } else if (turn === 1) {
         // re-enables selected card
         selectedCards.map((each) => each.removeAttribute("disabled"));
         selectedCards = [];
         selectedCardsId = [];
+        //change turn
         turn = 2;
         playerTurn.value = turn;
         ImgCard1.src = './img/back.png'; 
         ImgCard2.src = './img/back.png'; 
-    } else if (turn == 2) {
+    } else if (turn === 2) {
         // re-enables selected card
         selectedCards.map((each) => each.removeAttribute("disabled"));
         ImgCard1.src = './img/back.png'; 
         ImgCard2.src = './img/back.png'; 
         selectedCards = [];
         selectedCardsId = [];
+        //change turn
         turn = 1;
         playerTurn.value = turn;
     }
